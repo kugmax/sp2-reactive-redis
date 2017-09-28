22e705cacd2dafa8845cf6c704962ef424ade62d
maxim.kugatov <maxim.kugatov@digitalhouse.biz>
2017-09-28 17:31:05 +0300

    Initial commit

---
+++ src/main/java/com/kugmax/learn/sp2/reactiveredis/ReactiveRedisLearnApplication.java
@@ -0,0 +1,12 @@
+package com.kugmax.learn.sp2.reactiveredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveRedisLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveRedisLearnApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kugmax/learn/sp2/reactiveredis/ReactiveRedisLearnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kugmax.learn.sp2.reactiveredis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveRedisLearnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

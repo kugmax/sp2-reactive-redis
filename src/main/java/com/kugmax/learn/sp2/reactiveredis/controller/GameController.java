fa76729a10ed4777f8cfbbacc8e121c492e66c63
maxim.kugatov <maxim.kugatov@digitalhouse.biz>
2017-10-02 14:06:54 +0300

    dont work 2

--- src/main/java/com/kugmax/learn/sp2/reactiveredis/controller/GameController.java
+++ src/main/java/com/kugmax/learn/sp2/reactiveredis/controller/GameController.java
@@ -1,14 +1,28 @@
 package com.kugmax.learn.sp2.reactiveredis.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kugmax.learn.sp2.reactiveredis.dao.GameDao;
+import com.kugmax.learn.sp2.reactiveredis.model.Game;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
-public class HelloController {
+public class GameController {
 
-    @RequestMapping("/")
-    public Mono<String> hello() {
-        return Mono.just("Hello");
+    private GameDao gameDao;
+
+    @Autowired
+    public GameController(GameDao gameDao) {
+        this.gameDao = gameDao;
+    }
+
+    @RequestMapping(value = "/game/{id}", method = RequestMethod.GET)
+    public Mono<Game> getGame(@PathVariable("id") long id ) {
+        return gameDao.get(id);
+    }
+
+    @RequestMapping(value = "/game/", method = RequestMethod.PUT)
+    public void save(@RequestBody Game game) {
+        gameDao.put(game);
     }
 }
--- src/main/java/com/kugmax/learn/sp2/reactiveredis/dao/GameDaoRedis.java
+++ src/main/java/com/kugmax/learn/sp2/reactiveredis/dao/GameDaoRedis.java
@@ -1,23 +1,32 @@
 package com.kugmax.learn.sp2.reactiveredis.dao;
 
 import com.kugmax.learn.sp2.reactiveredis.model.Game;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.connection.ReactiveHashCommands;
-import org.springframework.data.redis.core.ReactiveHashOperations;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.charset.CharsetEncoder;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collector;
 
 @Repository
 public class GameDaoRedis implements GameDao {
+    private static final Log log = LogFactory.getLog(GameDaoRedis.class);
 
     public static final String BASE_KEY = "com:kugmax:sp2:reactive:game:";
+
+    @Value("${dao.game.redis.ttl.seconds}")
+    public long TTL_SECONDS;
 
     private ReactiveRedisTemplate template;
 
@@ -28,44 +37,63 @@ public class GameDaoRedis implements GameDao {
 
     @Override
     public void put(Game game) {
-//        ByteBuffer key =  ByteBuffer.wrap(content.getBytes("UTF-8")) ;
-//
-//        ByteBuffer key = stringBuffer;
-//        CharBuffer f;
-//
-//        Map<ByteBuffer, ByteBuffer> fields = new HashMap<>();
-//        fields.put("name", game.getName());
-//        fields.put("description", game.getDescription());
-//
-//        template.execute( reactiveRedisConnection -> {
-//            ReactiveHashCommands reactiveHashCommands = reactiveRedisConnection.hashCommands();
-//            reactiveHashCommands.hMSet(key, fields);
-//
-//
-//            }
-//        );
+        log.info("### game " + game);
 
+        ByteBuffer key = buildKey(game.getGameID());
 
-//        String content = "It's easy to convert ByteBuffer to String in Java";
-//        ByteBuffer buffer = ByteBuffer.wrap(content.getBytes("UTF-8"));
-//        String converted = new String(buffer.array(), "UTF-8");
-//        System.out.println("Original : " + content);
-//        System.out.println("Converted : " + converted);
+        Map<ByteBuffer, ByteBuffer> fields = new HashMap<>();
+        fields.put( wrap("name"), wrap(game.getName()) );
+        fields.put( wrap("description"), wrap(game.getDescription()) );
 
+        Duration ttl = Duration.ofSeconds(TTL_SECONDS);
+
+        Flux<?> result = template.execute( reactiveRedisConnection -> {
+            ReactiveHashCommands reactiveHashCommands = reactiveRedisConnection.hashCommands();
+            reactiveHashCommands.hMSet(key, fields);
+            reactiveRedisConnection.keyCommands().expire(key, ttl);
+            return Flux.empty();
+            }
+        );
+
+        result.subscribe();
     }
 
     @Override
     public Mono<Game> get(long id) {
-//        template.createMono(reactiveRedisConnection -> reactiveRedisConnection. );
+        Flux<Map.Entry> entriesGame = template.opsForHash().entries(buildKey(id));
 
+        return entriesGame.collect(
+                Collector.of(
+                        Game::new,
+                        this::persist,
+                        (game, game2) -> game,
+                        Function.identity()
+                )
+        );
+    }
 
-        return null;
+    private void persist(Game game, Map.Entry<String, String> entry) {
+        if ("name".equals(entry.getKey()) ) {
+            game.setName(entry.getValue());
+
+        } else if ("description".equals(entry.getKey())) {
+            game.setDescription(entry.getValue());
+        } else {
+            log.error("Unsupported entry " + entry);
+        }
     }
 
     private ByteBuffer buildKey(long id) {
-//        return ByteBuffer.wrap( (BASE_KEY + String.valueOf(id)).getBytes("UTF-8")) ;
-        return null;
+        return wrap (BASE_KEY + String.valueOf(id));
     }
 
+    private ByteBuffer wrap(String txt) {
+        try {
+            return ByteBuffer.wrap(txt.getBytes("UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            log.error("Nice api spring-data ((((( " + e.getMessage(), e);
+        }
 
+        return null; //NullPointerException
+    }
 }
